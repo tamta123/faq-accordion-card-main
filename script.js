5c5866e324d5fe177e9648dbbcbbe25edb016641
tamta123 <tamtag186@gmail.com>
2023-04-07 11:10:41 +0400

    style corrections

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 
-const arrowUp = document.querySelectorAll('.arrowup');
 const arrowDown = document.querySelectorAll('.arrowdown');
-const answer = document.querySelectorAll('p');
+const questions = document.querySelectorAll('.question')
 
-for (let i=0; i<arrowUp.length; i++) {
+
+for (let i=0; i<arrowDown.length; i++) {
     arrowDown[i].addEventListener('click',function() {
-arrowUp[i].style.display = 'block';
-arrowDown[i].style.display = 'none';
-answer[i].style.display = 'block'; })
-
-arrowUp[i].addEventListener('click', function(){
-    arrowUp[i].style.display = 'none'
-    arrowDown[i].style.display = 'block';
-    answer[i].style.display = 'none'
-})
-}
+        questions[i].classList.toggle('active')
+    })}
